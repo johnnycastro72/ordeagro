86cc85208fa45d5c2c409aa11272eb241b9374c3
Jhonny Castro Clavijo <johnny.castro@misena.edu.co>
2022-05-19 17:27:05 -0500

    Initial commit

---
+++ src/main/java/co/com/sofka/ordeagro/OrdeagroApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.ordeagro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdeagroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdeagroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/com/sofka/ordeagro/OrdeagroApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.ordeagro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdeagroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
